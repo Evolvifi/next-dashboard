47c241076ddfaac42a49baf3ca4ba113ee2c2b7a
terranceweeden <terranceweeden@gmail.com>
2026-01-11 10:21:10 -0600

    added metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,6 @@
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchCardData } from "@/app/lib/data";
 import { Suspense } from "react";
 import CardWrapper from "@/app/ui/dashboard/cards";
 import {
@@ -9,6 +8,12 @@ import {
   LatestInvoicesSkeleton,
   CardsSkeleton,
 } from "@/app/ui/skeletons";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 export default async function Page() {
   return (
